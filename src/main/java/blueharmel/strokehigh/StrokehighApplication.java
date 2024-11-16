3794807b1d385415becb473d072d49d36eef2223
Blueharmel <dldjdtjr@gmail.com>
2024-11-17 00:19:06 +0900

    초기 커밋

---
+++ src/main/java/blueharmel/strokehigh/StrokehighApplication.java
@@ -0,0 +1,13 @@
+package blueharmel.strokehigh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrokehighApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrokehighApplication.class, args);
+    }
+
+}
---
+++ src/test/java/blueharmel/strokehigh/StrokehighApplicationTests.java
@@ -0,0 +1,13 @@
+package blueharmel.strokehigh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrokehighApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
